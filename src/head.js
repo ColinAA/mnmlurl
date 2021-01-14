874fe65c28a1662d0f09defe9b27e8d7b1f2e15c
ColinAA <33395983+ColinAA@users.noreply.github.com>
2021-01-14 12:16:10 +0100

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://www.jsonstore.io/6b6e3cd07f3aaecd8df25ab95875eb2462f00a7614f7205c2d5dc5229896ea98'
+const endpoint = 'https://api.jsonbin.io/$2b$10$dNJ5.hB3bkzfnZTftxiPcO3Oz8CMu2WwiyIC2wCl8j65F0QJmOmty'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
 	const f = new XMLHttpRequest()
